3f6b3862b8d67512ea2b0ede48712e11ee719cdc
kammradt <vinicius.kammradt1@gmail.com>
2021-07-26 00:52:32 -0300

    :sparkles: adds twitter dag

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -2,5 +2,6 @@
 
 airflow db init
 airflow users create --username admin --password admin --role Admin --firstname admin --lastname admin --email admin@email.com
-airflow scheduler &
-exec airflow webserver
+airflow scheduler & 
+sleep 5
+airflow webserver
